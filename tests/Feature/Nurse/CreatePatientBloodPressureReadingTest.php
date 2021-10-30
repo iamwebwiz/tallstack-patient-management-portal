3bd562d85b25e27daebb3cd1ef2d929fb09f1e04
Ezekiel <iamwebwiz@gmail.com>
2021-10-31 00:57:46 +0100

    more tests

--- tests/Feature/Nurse/CreatePatientBloodPressureReadingTest.php
+++ tests/Feature/Nurse/CreatePatientBloodPressureReadingTest.php
@@ -7,11 +7,12 @@ use App\Models\Patient;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Livewire\Livewire;
 use Tests\TestCase;
 
 class CreatePatientBloodPressureReadingTest extends TestCase
 {
-    use RefreshDatabase;
+    use RefreshDatabase, WithFaker;
 
     /** @test */
     public function a_nurse_can_view_page_to_create_new_blood_pressure_entry()
@@ -25,5 +26,36 @@ class CreatePatientBloodPressureReadingTest extends TestCase
             ->assertSuccessful()
             ->assertSeeText('New Blood Pressure Reading')
             ->assertSeeLivewire(CreateNewBloodPressureEntry::class);
+    }
+
+    /** @test */
+    public function valid_data_must_be_sent_for_processing()
+    {
+        Livewire::actingAs(User::factory()->create(['role' => 'nurse']))
+            ->test(CreateNewBloodPressureEntry::class, ['patient' => Patient::factory()->create()])
+            ->call('submit')
+            ->assertHasErrors([
+                'diastolic' => 'required',
+                'systolic' => 'required',
+                'pulsePressure' => 'required',
+            ]);
+    }
+
+    /** @test */
+    public function a_nurse_can_add_new_blood_pressure_reading_for_patient()
+    {
+        $patient = Patient::factory()->create();
+
+        $this->assertDatabaseCount('patient_blood_pressures', 0);
+
+        Livewire::actingAs(User::factory()->create(['role' => 'nurse']))
+            ->test(CreateNewBloodPressureEntry::class, ['patient' => $patient])
+            ->set('diastolic', $this->faker->randomNumber(3))
+            ->set('systolic', $this->faker->randomNumber(3))
+            ->set('pulsePressure', $this->faker->randomNumber())
+            ->call('submit')
+            ->assertRedirect(route('patients.readings', $patient->id));
+
+        $this->assertDatabaseCount('patient_blood_pressures', 1);
     }
 }
